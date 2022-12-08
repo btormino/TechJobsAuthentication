14ad410f04b7e6dcfa540710f16bb9479b03d535
Bethany <morphforlife@gmail.com>
2022-12-08 06:10:48 -0600

    Created a scaffold identity, configured the validation conditions needed, added authorization attributes necessary.

--- TechJobsAuthentication/Data/JobDbContext.cs
+++ TechJobsAuthentication/Data/JobDbContext.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TechJobsAuthentication.Models;
 
 namespace TechJobsAuthentication.Data
 {
-    public class JobDbContext : DbContext
+    public class JobDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public DbSet<Job> Jobs { get; set; }
 
         public JobDbContext(DbContextOptions<JobDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Job>().HasKey(et => new { et.Id, et.Skill });
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
